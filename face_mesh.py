2b21090645ff340ded034b0f6b3f09b462566a76
katsunori-waragai <huyuhiko1128@gmail.com>
2023-11-07 15:54:06 +0900

    reformat by black

--- face_mesh.py
+++ face_mesh.py
@@ -5,8 +5,10 @@ mediapipe を用いて顔のメッシュを描画するサンプルスクリプ�
 
 import cv2
 import mediapipe as mp
+
 if __name__ == "__main__":
     import argparse
+
     parser = argparse.ArgumentParser(description="face_mash")
     parser.add_argument("video", help="video_file")
     args = parser.parse_args()
@@ -27,43 +29,43 @@ if __name__ == "__main__":
         max_num_faces=1,
         refine_landmarks=True,
         min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as face_mesh:
-      while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-          break
-        image.flags.writeable = False
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = face_mesh.process(image)
+        min_tracking_confidence=0.5,
+    ) as face_mesh:
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                break
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = face_mesh.process(image)
 
-        # 検出された顔のメッシュをカメラ画像の上に描画
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        if results.multi_face_landmarks:
-          for face_landmarks in results.multi_face_landmarks:
-            mp_drawing.draw_landmarks(
-                image=image,
-                landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACEMESH_TESSELATION,
-                landmark_drawing_spec=None,
-                connection_drawing_spec=mp_drawing_styles
-                .get_default_face_mesh_tesselation_style())
-            mp_drawing.draw_landmarks(
-                image=image,
-                landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACEMESH_CONTOURS,
-                landmark_drawing_spec=None,
-                connection_drawing_spec=mp_drawing_styles
-                .get_default_face_mesh_contours_style())
-            mp_drawing.draw_landmarks(
-                image=image,
-                landmark_list=face_landmarks,
-                connections=mp_face_mesh.FACEMESH_IRISES,
-                landmark_drawing_spec=None,
-                connection_drawing_spec=mp_drawing_styles
-                .get_default_face_mesh_iris_connections_style())
-        cv2.imshow('MediaPipe Face Mesh', cv2.flip(image, 1))
-        if cv2.waitKey(5) & 0xFF == 27:
-          break
+            # 検出された顔のメッシュをカメラ画像の上に描画
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.multi_face_landmarks:
+                for face_landmarks in results.multi_face_landmarks:
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_TESSELATION,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style(),
+                    )
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_CONTOURS,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style(),
+                    )
+                    mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=mp_face_mesh.FACEMESH_IRISES,
+                        landmark_drawing_spec=None,
+                        connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_iris_connections_style(),
+                    )
+            cv2.imshow("MediaPipe Face Mesh", cv2.flip(image, 1))
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
     cap.release()
-
